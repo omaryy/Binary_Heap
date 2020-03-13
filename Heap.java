0a1b58f390337e6ff5f2cbbbb65c862e2aaf92dc
nada-fathy <48325643+nada-fathy@users.noreply.github.com>
2020-03-13 16:22:48 +0200

    Update Heap.java

--- Heap.java
+++ Heap.java
@@ -2,24 +2,28 @@ package eg.edu.alexu.csd.filestructure.sort;
 import java.lang.*;
 import java.util.Collection;
 
-public class Heap <Int extends Comparable<Int>>implements IHeap<Int>{
-	private int size=0;
-	private INode<Int>[]arr=(INode<Int>[])new INode[100];
+public class Heap <T extends Comparable<T>>implements IHeap<T>{
+	private static int size=0;
+	private  static INode[]arr=(INode[])new INode[10000000];
 	 Heap() {
-		
+		 arr=(INode<T>[])new INode[10000000];
+		 size=0;
 	}
-	public static  Heap ob=new Heap();
-	public static Heap getInst() {
-		if(ob==null) {
-			return new Heap();
-		}
-		return ob;
-	}
-	public INode<Int> getRoot() {
+	 Heap(int s){
+		 
+	 }
+	//public static  Heap ob=new Heap();
+	//public static Heap getInst() {
+	//	if(ob==null) {
+	//		return new Heap();
+	//	}
+	//	return ob;
+	//}
+	public INode<T> getRoot() {
 		if(size==0) {
 			return null;
 		}
-		return arr[1];
+		return (INode<T>) arr[1];
 	}
 
 	public int size() {
@@ -27,22 +31,25 @@ public class Heap <Int extends Comparable<Int>>implements IHeap<Int>{
 	}
 
 	
-	public void heapify(INode<Int> node) {
-		INode<Int>l=node.getLeftChild();
-		INode<Int>r=node.getRightChild();
-		INode<Int>large=node;
+	public void heapify(INode<T> node) {
+		if(node==null) {
+			return;
+		}
+		INode<T>l=node.getLeftChild();
+		INode<T>r=node.getRightChild();
+		INode<T>large=node;
 		//int left=((Integer) l.getValue()).intValue();
 		//int right=((Integer) r.getValue()).intValue();
-		int n=((Integer) node.getValue()).intValue();
-		if(l!=null &&((Integer) l.getValue()).intValue()>n ) {
+		T n=node.getValue();
+		if(l!=null &&( l.getValue().compareTo(n)>0)) {
 			large=l;
 		}
-		if(r!=null && ((Integer) r.getValue()).intValue()>((Integer) node.getValue()).intValue()) {
+		if(r!=null && ( r.getValue().compareTo(node.getValue())>1)) {
 			large=r;
 		}
 		
 		if(large!=node) {
-			Int x=node.getValue();
+			T x=node.getValue();
 			node.setValue(large.getValue());
 			large.setValue(x);
 			heapify(large);
@@ -51,56 +58,65 @@ public class Heap <Int extends Comparable<Int>>implements IHeap<Int>{
 	}
 
 	@Override
-	public Int extract() {
-		Int temp=arr[1].getValue();
+	public T extract() {
+		if(size==0) {
+			return null;
+		}
+		T temp=(T) arr[1].getValue();
 		arr[1].setValue(arr[size].getValue());
 		size--;
-		heapify(arr[1]);
-		for(int i=1;i<=size;i++) {
-			System.out.println(arr[i].getValue());
-		}
+		heapify((INode<T>) arr[1]);
 		return temp;
 	}
 
-	public void insert(Int element) {
-		Node<Int> n=new Node<Int>(element,size+1);
+	public void insert(T element) {
+		Node<T> n=new Node<T>(element,size+1);
 		if(size==arr.length) {
 			System.out.println("Maximum capacity");
 			return;
 		}
-		arr[size+1]=n;
+		arr[size+1]=(INode<T>) n;
 		size++;
 		int i=size;
 		while(i!=1&&i!=0 &&(((Integer) arr[i].getValue()).intValue())>(((Integer) arr[i/2].getValue()).intValue())) {
-			Int temp=arr[i].getValue();
+			T temp=(T) arr[i].getValue();
 			arr[i].setValue(arr[i/2].getValue());
-			arr[i/2].setValue(temp);
-			System.out.println("element"+" "+arr[i].getValue()+"swaped"+arr[i/2].getValue());
+			arr[i/2].setValue((Integer) temp);
+			//System.out.println("element"+" "+arr[i].getValue()+"swaped"+arr[i/2].getValue());
 			i=i/2;
 		}
-		for(i=1;i<=size;i++) {
-			System.out.println(arr[i].getValue());
-		}
-		
+
 	}
 
-	public void build(Collection<Int> unordered) {
-		arr=(INode<Int>[])new INode[100];
+	public void build(Collection<T> unordered) {
+		if(unordered==null) {
+			size=0;
+			return;
+		}
+		arr=(INode<T>[])new INode[10000000];
 		size=0;
-		for(Int E:unordered) {
-			insert(E);
+		for(T E:unordered) {
+			Node<T> n=new Node<T>(E,size+1);
+			arr[size+1]=(INode<T>) n;
+			size++;
 		}
-		int end=unordered.size()/2;
+		int end=size;
 		for(int i=end;i>0;i--) {
-			heapify(arr[i]);
-		}
-		for(int i=1;i<=size;i++) {
-			System.out.println(arr[i].getValue());
+			heapify((INode<T>) arr[i]);
 		}
 		
 	}
-	public INode<Int>[] getHeap(){
-		return arr;
+	public INode<T>[] getHeap(){
+		return (INode<T>[]) arr;
+	}
+	public void add(T element) {
+		Node<T> n=new Node<T>(element,size+1);
+		if(size==arr.length) {
+			System.out.println("Maximum capacity");
+			return;
+		}
+		arr[size+1]=(INode<T>) n;
+		size++;
 	}
 	
 }
